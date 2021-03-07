373ae7a4193dcd9fa52d557a1ad37ab15099734f
Goudouras Evangelos <goudourasv@gmail.com>
2021-03-07 19:38:46 +0200

    Add lear text file

---
+++ src/WordCount.java
@@ -0,0 +1,14 @@
+/* File:WordCount.java
+ *--------------
+ *This program  reads a file and reports how many
+ * lines, words, and characters appear  in  it.*/
+
+import acm.program.ConsoleProgram;
+import acm.util.*;
+import java.io.*;
+import java.util.ArrayList;
+
+
+public class WordCount extends ConsoleProgram {
+
+}
