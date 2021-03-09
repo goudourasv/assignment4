c605ef1c59e58deda7b830916bdba171584b7552
Goudouras Evangelos <goudourasv@gmail.com>
2021-03-09 12:58:24 +0200

    Implement UniqueNames in section assignment 5

---
+++ src/UniqueNames.java
@@ -0,0 +1,35 @@
+import acm.program.ConsoleProgram;
+import java.util.ArrayList;
+
+public class UniqueNames extends ConsoleProgram {
+    private ArrayList nameList;
+    private static final String SENTINEL = "";
+
+    public UniqueNames() {
+        nameList = new ArrayList();
+    }
+
+    public void run() {
+        while (true) {
+            String name = readLine("Enter a name: ");
+            if (name.equals(SENTINEL)) {
+                println("Unique name list contains: ");
+                printUniqueNameList();
+                break;
+            }
+            addNameToTheList(name);
+        }
+    }
+
+    private void printUniqueNameList() {
+        for (int i=0; i<nameList.size(); i++){
+            println(nameList.get(i));
+        }
+    }
+
+    private void addNameToTheList(String name) {
+        if (!nameList.contains(name)) {
+            nameList.add(name);
+        }
+    }
+}
